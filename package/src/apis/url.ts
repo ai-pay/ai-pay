b11bff57565f58a5a6c8c46919ad8fd324aa19cc
Marchusness <m_gunny31@outlook.com>
2024-04-07 23:16:01 +0930

    updated the documentation website with the new ask ai package

--- package/src/apis/url.ts
+++ package/src/apis/url.ts
@@ -6,3 +6,5 @@ export function setRootUrl(url: string): void {
 }
 
 export function AiUsageUrl(): string { return RootUrl + "/api/ai-usage" }
+
+export function UrlFromAiPayRootUrl(path: string): string { return RootUrl + path }
--- package/src/methods/index.ts
+++ package/src/methods/index.ts
@@ -1,2 +1,13 @@
+import { UrlFromAiPayRootUrl } from "@/apis/url";
+
 export { requestAiPayUsage } from "./requestAiPayUsage";
 export { setDebugLevel } from "./debugLogs";
+
+export type FreeTrialInformation = {
+    usd: number
+    numberCredits: number
+}
+
+export function getFreeTrialInformation(): Promise<FreeTrialInformation> {
+  return fetch(UrlFromAiPayRootUrl("/api/free-trial")).then(res => res.json())
+}
